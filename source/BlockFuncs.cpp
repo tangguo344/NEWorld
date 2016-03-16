4f551c0ba80167fa4b2081877aaf934da569d3ab
DLaboratory <dlaboratory@126.com>
2016-03-16 22:54:08 +0800

    用表驱动改写WaterBUF(Blocks::BUDPP*)
    把一段同样功能的代码复制四遍是搞什么鬼，差评！

--- source/BlockFuncs.cpp
+++ source/BlockFuncs.cpp
@@ -11,25 +11,27 @@ bool GrassBUF(Blocks::BUDDP * args)
     long long bx = args->cx;
     long long by = args->cy;
     long long bz = args->cz;
-    block b;
-    b = World::getblock(bx, by + 1, bz);
+    block b = World::getblock(bx, by + 1, bz);
     if (b.ID != Blocks::AIR)
     {
-        *(args->slf)=block(Blocks::DIRT);
+        *(args->slf) = block(Blocks::DIRT);
         return true;
     }
-    return false;
+    else
+    {
+        return false;
+    }
 }
 
 bool WaterBUF(Blocks::BUDDP * args)
 {
-    cout<<"callwith:"<< args->slf->Data81<<" "<< args->slf->Data82<<endl;
-    if (args->slf->Data81==1)
+    cout << "callwith:" << args->slf->Data81 << " " << args->slf->Data82 << endl;
+    if (args->slf->Data81 == 1)
     {
         long long bx = args->cx;
         long long by = args->cy;
         long long bz = args->cz;
-        block b=World::getblock(bx, by - 1, bz);
+        block b = World::getblock(bx, by - 1, bz);
         if (b.ID == Blocks::AIR)
         {
             World::Modifyblock(bx, by - 1, bz, block(Blocks::WATER, 1, args->slf->Data82));
@@ -37,70 +39,42 @@ bool WaterBUF(Blocks::BUDDP * args)
         }
         else if (b.ID == Blocks::WATER)
         {
-            int s = b.Data82 + args->slf->Data82;
-            if (s>255) s = 255;
-            if (s>b.Data82)
+            int s = min(b.Data82 + args->slf->Data82, 255);
+            if (s > b.Data82)
             {
                 World::Modifyblock(bx, by - 1, bz, block(Blocks::WATER, 1, s));
                 return true;
             }
-            else return false;
-        }
-        if (args->slf->Data82 < 16) return false;
-        bool set=false;
-        b = World::getblock(bx + 1, by, bz);
-        cout<<b.ID<<" ";
-        if (b.ID == Blocks::AIR)
-        {
-            World::Modifyblock(bx + 1, by, bz, block(Blocks::WATER, 1, args->slf->Data82 - 16));
-            set = true;
-        }
-        else if (b.ID == Blocks::WATER)
-            if (b.Data82<args->slf->Data82 - 16)
+            else
             {
-                World::Modifyblock(bx + 1, by, bz, block(Blocks::WATER, 1, args->slf->Data82 - 16));
+                return false;
+            }
+        }
+        if (args->slf->Data82 < 16)
+            return false;
+
+        bool set = false;
+        const int vec[] = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+        for(int i = 0; i < 4; i++)
+        {
+            b = World::getblock(bx + vec[i][0], by, bz + vec[i][1]);
+            cout << b.ID << " ";
+            if (b.ID == Blocks::AIR)
+            {
+                World::Modifyblock(bx + vec[i][0], by, bz + vec[i][1], block(Blocks::WATER, 1, args->slf->Data82 - 16));
                 set = true;
             }
-        b = World::getblock(bx - 1, by, bz);
-        cout << b.ID << " ";
-        if (b.ID == Blocks::AIR)
-        {
-            World::Modifyblock(bx - 1, by, bz, block(Blocks::WATER, 1, args->slf->Data82 - 16));
-            set = true;
-        }
-        else if (b.ID == Blocks::WATER)
-            if (b.Data82<args->slf->Data82 - 16)
+            else if (b.ID == Blocks::WATER)
             {
-                World::Modifyblock(bx - 1, by, bz, block(Blocks::WATER, 1, args->slf->Data82 - 16));
-                set = true;
+                if (b.Data82 < args->slf->Data82 - 16)
+                {
+                    World::Modifyblock(bx + vec[i][0], by, bz + vec[i][1], block(Blocks::WATER, 1, args->slf->Data82 - 16));
+                    set = true;
+                }
             }
-        b = World::getblock(bx, by, bz + 1);
-        cout << b.ID << " ";
-        if (b.ID == Blocks::AIR)
-        {
-            World::Modifyblock(bx, by, bz + 1, block(Blocks::WATER, 1, args->slf->Data82 - 16));
-            set = true;
         }
-        else if (b.ID == Blocks::WATER)
-            if (b.Data82<args->slf->Data82 - 16)
-            {
-                World::Modifyblock(bx, by, bz + 1, block(Blocks::WATER, 1, args->slf->Data82 - 16));
-                set = true;
-            }
-        b = World::getblock(bx, by, bz - 1);
-        cout << b.ID << endl;
-        if (b.ID == Blocks::AIR)
-        {
-            World::Modifyblock(bx, by, bz - 1, block(Blocks::WATER, 1, args->slf->Data82 - 16));
-            set = true;
-        }
-        else if (b.ID == Blocks::WATER)
-            if (b.Data82<args->slf->Data82 - 16)
-            {
-                World::Modifyblock(bx, by, bz - 1, block(Blocks::WATER, 1, args->slf->Data82 - 16));
-                set = true;
-            }
-        if (set) return true;
+        if (set)
+            return true;
     }
     return false;
 }
