5b8308f69124352ad24038431077949e3713b55f
ascchrvalstr <ascchrvalstr@sina.com>
2016-04-24 19:49:34 +0800

    Add UNIX symbol resolving feature
    It is *not* guaranteed that it will work.

--- PluginSDK/src/defs.h
+++ PluginSDK/src/defs.h
@@ -3,19 +3,50 @@
 
 #ifdef _WIN32
 #include <Windows.h>
+
 typedef HMODULE dll_handle;
+
 inline dll_handle load_library(wchar_t const * const file_name)
 {
     return LoadLibraryW(file_name);
 }
+
 inline void* get_address(dll_handle handle, char const * const symbol_name)
 {
     return GetProcAddress(handle, symbol_name);
 }
+
 inline void unload_library(dll_handle handle)
 {
     FreeLibrary(handle);
 }
+
+#elif defined(__unix__) || defined(unix)
+#include <dlfcn.h>
+
+typedef void* dll_handle;
+
+inline dll_handle load_library(wchar_t const * const file_name)
+{
+    size_t len = wcslen(file_name);
+    unique_ptr<char> tmp(new char[len * 3]);
+    wcstombs(tmp.get(), file_name, len);
+    return dlopen(tmp.get(), RTLD_DEFAULT);
+}
+
+inline void* get_address(dll_handle handle, char const * const symbol_name)
+{
+    size_t len = wcslen(symbol_name);
+    unique_ptr<char> tmp(new char[len * 3]);
+    wcstombs(tmp.get(), symbol_name, len);
+    return dlsym(handle, tmp.get());
+}
+
+inline void unload_library(dll_handle handle)
+{
+    dlclose(handle);
+}
+
 #endif
 
 #include <string>
