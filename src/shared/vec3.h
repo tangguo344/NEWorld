d2044b01ce70d7378bb30986d8182d0294aff452
qiaozhanrong <qiaozhanrong@163.com>
2016-05-07 23:49:16 +0800

    Update class Vec3

--- src/shared/vec3.h
+++ src/shared/vec3.h
@@ -33,25 +33,17 @@ public:
         x = rhs.x; y = rhs.y; z = rhs.z;
     }
 
-    bool operator== (const Vec3& rhs) const
+    bool operator< (const Vec3& rhs) const
     {
-        return x == rhs.x && y == rhs.y && z == rhs.z;
+        if (x < rhs.x) return true; if (x > rhs.x) return false;
+        if (y < rhs.y) return true; if (y > rhs.y) return false;
+        if (z < rhs.z) return true; return false;
     }
 
-    bool operator!= (const Vec3& rhs) const
-    {
-        return x != rhs.x || y != rhs.y || z != rhs.z;
-    }
-
-    Vec3 operator+ (const Vec3& rhs) const
-    {
-        return Vec3(x + rhs.x, y + rhs.y, z + rhs.z);
-    }
-
-    Vec3 operator- (const Vec3& rhs) const
-    {
-        return Vec3(x - rhs.x, y - rhs.y, z - rhs.z);
-    }
+    bool operator== (const Vec3& rhs) const { return x == rhs.x && y == rhs.y && z == rhs.z; }
+    bool operator!= (const Vec3& rhs) const { return x != rhs.x || y != rhs.y || z != rhs.z; }
+    Vec3 operator+ (const Vec3& rhs) const { return Vec3(x + rhs.x, y + rhs.y, z + rhs.z); }
+    Vec3 operator- (const Vec3& rhs) const { return Vec3(x - rhs.x, y - rhs.y, z - rhs.z); }
 
 };
 
