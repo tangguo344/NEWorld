d218b4545e2cd77c3d2403fbb99740a47c9f3630
qiaozhanrong <qiaozhanrong@163.com>
2016-05-07 22:58:32 +0800

    License

--- src/shared/world.h
+++ src/shared/world.h
@@ -1,9 +1,27 @@
+/*
+* NEWorld: An free game with similar rules to Minecraft.
+* Copyright (C) 2016 NEWorld Team
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef WORLD_H_
 #define WORLD_H_
 
 #include <algorithm>
 #include <string>
-#include <cstring> // malloc, realloc
+#include <cstring> // malloc, realloc, free
 
 #include "common.h"
 #include "chunk.h"
@@ -42,8 +60,8 @@ public:
     // World name
     std::string name;
 
-    // Initializes world
     World();
+    ~World();
 
     // Add chunk
     Chunk* addChunk(const Vec3& chunkPos);
