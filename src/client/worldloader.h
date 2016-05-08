cd447820ffb070bd17bfad8778a60dc6a8fb06c6
qiaozhanrong <qiaozhanrong@163.com>
2016-05-08 15:25:58 +0800

    Replace & License

---
+++ src/client/worldloader.h
@@ -0,0 +1,47 @@
+/*
+* NEWorld: An free game with similar rules to Minecraft.
+* Copyright (C) 2016 NEWorld Team
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef WORLDLOADER_H_
+#define WORLDLOADER_H_
+
+#include <utility>
+#include "world.h"
+using std::pair;
+
+const int MaxChunkLoadCount = 64;
+const int MaxChunkUnloadCount = 64;
+
+class WorldLoader
+{
+    private:
+        World* _world;
+        int _chunkLoadCount, _chunkUnloadCount;
+        pair<Vec3, int> _chunkLoadList[256]; // Chunk load list <position, distance>
+        pair<Chunk*, int> _chunkUnloadList[256]; // Chunk unload list <pointer, distance>
+
+    public:
+        WorldLoader(World* _world_) :_world(_world_) {}
+
+        // Find the nearest chunks in load distance to load, fartherest chunks out of load distance to unload
+        void sortChunkLoadUnloadList(const Vec3& centerPos);
+        // Load & Unload chunks
+        void loadUnloadChunks();
+
+};
+
+#endif // !WORLDLOADER_H_
