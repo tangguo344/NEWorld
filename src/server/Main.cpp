7d69ce85f0e72405162717f440c2428a512fde6c
Null <abc612008@126.com>
2016-05-29 10:38:21 +0800

    Add time

--- src/server/Main.cpp
+++ src/server/Main.cpp
@@ -22,6 +22,7 @@
 #include <thread>
 #include <utility>
 #include <queue>
+#include <chrono>
 using namespace boost::asio;
 using namespace boost::system;
 using namespace boost::posix_time;
@@ -204,10 +205,20 @@ private:
     tcp::acceptor m_acceptor;
     tcp::socket m_socket;
 };
-
+const std::string getCurrentSystemTime()
+{
+    auto tt = std::chrono::system_clock::to_time_t
+              (std::chrono::system_clock::now());
+    struct tm* ptm = localtime(&tt);
+    char date[60] = { 0 };
+    sprintf(date, "%d-%02d-%02d %02d:%02d:%02d",
+            (int)ptm->tm_year + 1900, (int)ptm->tm_mon + 1, (int)ptm->tm_mday,
+            (int)ptm->tm_hour, (int)ptm->tm_min, (int)ptm->tm_sec);
+    return std::string(date);
+}
 int main(int argc, char* argv[])
 {
-    infostream << "System time:" << logendl;
+    infostream << "System time: " << getCurrentSystemTime() << logendl;
     infostream << "Server is starting..." << logendl;
     infostream << "Server started" << logendl;
     try
