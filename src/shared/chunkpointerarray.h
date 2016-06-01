36b36bfc98a6d31a7f7d38a4409e438d1f2f97a9
ascchrvalstr <ascchrvalstr@sina.com>
2016-06-01 17:00:42 +0800

    arrange code

--- src/shared/chunkpointerarray.h
+++ src/shared/chunkpointerarray.h
@@ -27,11 +27,11 @@ class ChunkPointerArray
 	:boost::noncopyable
 {
 private:
-    // Array
+    /// Array
     Chunk** m_array;
-    // Array size
+    /// Array size
     int m_size, m_size2, m_size3;
-    // Origin
+    /// Origin
     Vec3i m_org;
 
 public:
@@ -43,36 +43,44 @@ public:
         m_array = new Chunk*[m_size3];
         memset(m_array, 0, m_size3*sizeof(Chunk*));
     }
+
     ~ChunkPointerArray()
     {
         delete[] m_array;
     }
 
-    // Move array by delta
-    void move(const Vec3i& delta);
-    // Move array to pos
+    /// Move array by delta
+    void move(const Vec3i& delta)
+    {
+        Chunk** arr = new Chunk*[m_size3];
+        Vec3i::for_range(0, m_size, [this, arr, delta](const Vec3i& pos)
+        {arr[pos.x*m_size2 + pos.y*m_size + pos.z] = elementExists(pos + delta) ? m_array[(pos.x + delta.x)*m_size2 + (pos.y + delta.y)*m_size + (pos.z + delta.z)] : nullptr; });
+        delete[] m_array;
+        m_array = arr;
+        m_org += delta;
+    }
+    /// Move array to pos
     void moveTo(const Vec3i& pos)
     {
         move(pos - m_org);
     }
-    // Check if specific element is inside array range
+    /// Check if specific element is inside array range
     bool elementExists(const Vec3i& pos) const
     {
         return pos.x >= 0 && pos.x < m_size && pos.z >= 0 && pos.z < m_size && pos.y >= 0 && pos.y < m_size;
     }
-    // Get chunk pointer from array
+    /// Get chunk pointer from array
     Chunk* getChunkPtr(Vec3i pos) const
     {
-        pos = pos - m_org;
-        if (!elementExists(pos)) return nullptr;
-        return m_array[pos.x*m_size2 + pos.y*m_size + pos.z];
+        pos -= m_org;
+        return elementExists(pos) ? m_array[pos.x*m_size2 + pos.y*m_size + pos.z] : nullptr;
     }
-    // Update chunk pointer in array
+    /// Update chunk pointer in array
     void setChunkPtr(Vec3i pos, Chunk* c)
     {
-        pos = pos - m_org;
-        if (!elementExists(pos)) return;
-        m_array[pos.x*m_size2 + pos.y*m_size + pos.z] = c;
+        pos -= m_org;
+        if (elementExists(pos))
+            m_array[pos.x*m_size2 + pos.y*m_size + pos.z] = c;
     }
 
 };
