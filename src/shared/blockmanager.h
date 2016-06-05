205d6a33ff92a883ac04300fe4462c4e456e936b
qiaozhanrong <qiaozhanrong@163.com>
2016-06-05 20:07:15 +0800

    class BlockManager

---
+++ src/shared/blockmanager.h
@@ -0,0 +1,39 @@
+/*
+* NEWorld: A free game with similar rules to Minecraft.
+* Copyright (C) 2016 NEWorld Team
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef BLOCKMANAGER_H_
+#define BLOCKMANAGER_H_
+
+#include <vector>
+using std::vector;
+#include "blocktype.h"
+
+class BlockManager
+{
+private:
+    vector<BlockType> blocks;
+
+public:
+    void registerBlock(const BlockType& block)
+    {
+        blocks.push_back(block);
+    }
+
+};
+
+#endif // !BLOCKMANAGER_H_
