12f3f4fa8ec264f5f16100ed6b81036521baec4e
Null <abc612008@126.com>
2016-06-11 12:03:01 +0800

    copyright

--- src/server/Main.cpp
+++ src/server/Main.cpp
@@ -20,12 +20,36 @@
 #include <boost/format.hpp>
 #include <logger.h>
 #include <utility>
+#include <string>
 #include "server.h"
+#include <common.h>
+
+std::string copyright()
+{
+    return "\n\
+NEWorld: A free game with similar rules to Minecraft.\n\
+ Copyright(C) 2016 NEWorld Team\n\
+\n\
+ This program is free software : you can redistribute it and/or modify\n\
+ it under the terms of the GNU General Public License as published by\n\
+ the Free Software Foundation, either version 3 of the License, or\n\
+(at your option) any later version.\n\
+\n\
+ This program is distributed in the hope that it will be useful,\n\
+but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the\n\
+ GNU General Public License for more details.\n\
+\n\
+ You should have received a copy of the GNU General Public License\n\
+ along with this program.If not, see <http://www.gnu.org/licenses/>. \n";
+
+}
 
 int main(int argc, char* argv[])
 {
     loggerInit();
-    infostream << "Server is starting...";
+    infostream << copyright();
+    infostream << "Server is starting... Version: " << NEWORLD_VERSION;
     infostream << "Server started";
     try
     {
--- src/shared/logger.h
+++ src/shared/logger.h
@@ -1,21 +1,20 @@
 /*
- * This file is part of NGWorld.
- * Then DLaboratory copied this file from NGWorld to NEWorld.
- * (C) Copyright 2016 DLaboratory
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+* NEWorld: A free game with similar rules to Minecraft.
+* Copyright (C) 2016 NEWorld Team
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef _LOGGER_H_
 #define _LOGGER_H_
