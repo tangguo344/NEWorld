d69714b8abf492a8bc5c15ed02687c737bdb721a
qiaozhanrong <qiaozhanrong@163.com>
2016-06-11 18:01:50 +0800

    Arrange code

--- src/shared/Common.h
+++ src/shared/Common.h
@@ -49,7 +49,7 @@
     #define WIN32_LEAN_AND_MEAN
     #include <Windows.h> // Windows API
 #else
-//    #include <pthread.h> // Or <thread> <mutex>
+    //    #include <pthread.h> // Or <thread> <mutex>
 #endif
 
 #include <cassert>
@@ -58,6 +58,24 @@
     #define NEWORLD_COMPILER_RSHIFT_ARITH // Arithmetic shift right
 #endif
 
-#define NEWORLD_VERSION 501
+#define NEWORLD_VERSION 41u;
+//constexpr unsigned int NEWorldVersion = 41u;
+
+constexpr char* CopyrightString = "\n\
+NEWorld: A free game with similar rules to Minecraft.\n\
+ Copyright(C) 2016 NEWorld Team\n\
+\n\
+ This program is free software : you can redistribute it and/or modify\n\
+ it under the terms of the GNU General Public License as published by\n\
+ the Free Software Foundation, either version 3 of the License, or\n\
+(at your option) any later version.\n\
+\n\
+ This program is distributed in the hope that it will be useful,\n\
+but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the\n\
+ GNU General Public License for more details.\n\
+\n\
+ You should have received a copy of the GNU General Public License\n\
+ along with this program.If not, see <http://www.gnu.org/licenses/>. \n";
 
 #endif // !COMMON_H_
