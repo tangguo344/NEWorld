c3e6241ca9eede36d6c518c2102eb2ec13b8321e
qiaozhanrong <qiaozhanrong@163.com>
2016-06-17 19:48:50 +0800

    Apply wheel

--- src/shared/precomp.h
+++ src/shared/precomp.h
@@ -20,35 +20,6 @@
 #define PRECOMP_H_
 
 #include "common.h"
-
-#include <boost/date_time/posix_time/posix_time.hpp>
-#include <boost/log/support/date_time.hpp>
-#include <boost/log/common.hpp>
-#include <boost/log/trivial.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/expressions/keyword.hpp>
-#include <boost/log/attributes.hpp>
-#include <boost/log/attributes/timer.hpp>
-#include <boost/log/sources/logger.hpp>
-#include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/sinks/sync_frontend.hpp>
-#include <boost/log/sinks/text_file_backend.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/console.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/attributes/named_scope.hpp>
-
-namespace logging = boost::log;
-namespace src = boost::log::sources;
-namespace expr = boost::log::expressions;
-namespace sinks = boost::log::sinks;
-namespace attrs = boost::log::attributes;
-namespace keywords = boost::log::keywords;
-
-#define debugstream BOOST_LOG_TRIVIAL(debug)     //给开发者看的信息
-#define infostream BOOST_LOG_TRIVIAL(info)       //给普通用户看的问题
-#define warningstream BOOST_LOG_TRIVIAL(warning) //可能影响功能、性能、稳定性但是不至于立刻崩溃的问题
-#define errorstream BOOST_LOG_TRIVIAL(error)     //影响游戏运行的问题
-#define fatalstream BOOST_LOG_TRIVIAL(fatal)     //无法恢复的错误
+#include "logger.h"
 
 #endif // !PRECOMP_H_
