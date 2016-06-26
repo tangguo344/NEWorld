8db6c50509951ef3a7cd4488e8f1b441700acbd9
qiaozhanrong <qiaozhanrong@163.com>
2016-06-26 12:52:03 +0800

    class Mat4 [Not Finished]

---
+++ src/client/mat4.h
@@ -0,0 +1,47 @@
+/*
+* NEWorld: A free game with similar rules to Minecraft.
+* Copyright (C) 2016 NEWorld Team
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef MAT4_H_
+#define MAT4_H_
+
+#include <cstring> // memset
+
+template <typename T>
+class Mat4
+{
+public:
+    T data[16];
+
+    Mat4()
+    { memset(data, 0, sizeof(data)); }
+    Mat4(const T& x)
+    {
+        memset(data, 0, sizeof(data));
+        data[0] = data[5] = data[10] = data[15] = x; // Unit matrix
+    }
+
+    // Get data by position
+    static int get(int x, int y)
+    { return data[y*4 + x]; }
+
+};
+
+using Mat4f = Mat4<float>;
+using Mat4d = Mat4<double>;
+
+#endif // !MAT4_H_
