ded4886449ca3d7413b5fab0e42fc3c9860ed9c0
qiaozhanrong <qiaozhanrong@163.com>
2016-07-08 18:46:10 +0800

    Change color code
    - Change color code into 0123456789abcdef
    - Change color order into [black, red, yellow, green, cyan, blue, magenta,
      white]
    - Add support for capitalized color code

--- src/server/servercommand.h
+++ src/server/servercommand.h
@@ -15,11 +15,11 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#ifndef _SERVERCOMMAND_H__
-#define _SERVERCOMMAND_H__
+#ifndef SERVERCOMMAND_H_
+#define SERVERCOMMAND_H_
 
 void inputThreadFunc();
 
 void stopInputThreadRunning();
 
-#endif //!_SERVERCOMMAND_H__
+#endif //!SERVERCOMMAND_H_
