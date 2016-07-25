169736297e663f24697fb9af33f9ac9fadded71f
Null <abc612008@126.com>
2016-07-25 11:21:04 +0800

    Change the license to LGPL

--- src/shared/common.h
+++ src/shared/common.h
@@ -2,18 +2,19 @@
 * NEWorld: A free game with similar rules to Minecraft.
 * Copyright (C) 2016 NEWorld Team
 *
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
+* This file is part of NEWorld.
+* NEWorld is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
 *
-* This program is distributed in the hope that it will be useful,
+* NEWorld is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
+* GNU Lesser General Public License for more details.
 *
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* You should have received a copy of the GNU Lesser General Public License
+* along with NEWorld.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef COMMON_H_
@@ -82,7 +83,7 @@ constexpr const char* CopyrightString = R"(
 NEWorld  Copyright (C) 2016  NEWorld Team
 This program comes with ABSOLUTELY NO WARRANTY.
 This is free software, and you are welcome to redistribute it under certain conditions.
-For details see "licenses/license_NEWorld.txt".
+For details see "LICENSE".
 )";
 
 #if defined(NEWORLD_TARGET_WINDOWS)
