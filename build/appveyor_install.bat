5807e4db8eb4ad82eb1defabb2711ed4fa77446a
Null <abc612008@users.noreply.github.com>
2016-08-26 10:32:37 -0700

    Appveyor x64 support

--- build/appveyor_install.bat
+++ build/appveyor_install.bat
@@ -10,8 +10,11 @@ nuget install boost_regex-vc140 -Version 1.61.0 > nul
 cd ..
 :: Install other dependencies
 echo Installing other dependencies...
+mkdir dependencies\
+cd dependencies\
 appveyor DownloadFile https://raw.githubusercontent.com/Infinideastudio/NEWorld-binaries/master/dependencies.exe
-C:\NEWorld\dependencies.exe > nul
+dependencies.exe > nul
+cd ..
 :: Link
 echo Creating links...
 mklink /D dependencies\include\boost C:\NEWorld\boost\boost.1.61.0.0\lib\native\include\boost
@@ -20,3 +23,9 @@ copy C:\NEWorld\boost\boost_filesystem-vc140.1.61.0.0\lib\native\address-model-3
 copy C:\NEWorld\boost\boost_system-vc140.1.61.0.0\lib\native\address-model-32\lib\libboost_system-vc140-mt-gd-1_61.lib dependencies\lib\boostlib
 copy C:\NEWorld\boost\boost_date_time-vc140.1.61.0.0\lib\native\address-model-32\lib\libboost_date_time-vc140-mt-gd-1_61.lib dependencies\lib\boostlib
 copy C:\NEWorld\boost\boost_regex-vc140.1.61.0.0\lib\native\address-model-32\lib\libboost_regex-vc140-mt-gd-1_61.lib dependencies\lib\boostlib
+
+mkdir dependencies\lib_x64\boostlib
+copy C:\NEWorld\boost\boost_filesystem-vc140.1.61.0.0\lib\native\address-model-64\lib\libboost_filesystem-vc140-mt-gd-1_61.lib dependencies\lib_x64\boostlib
+copy C:\NEWorld\boost\boost_system-vc140.1.61.0.0\lib\native\address-model-64\lib\libboost_system-vc140-mt-gd-1_61.lib dependencies\lib_x64\boostlib
+copy C:\NEWorld\boost\boost_date_time-vc140.1.61.0.0\lib\native\address-model-64\lib\libboost_date_time-vc140-mt-gd-1_61.lib dependencies\lib_x64\boostlib
+copy C:\NEWorld\boost\boost_regex-vc140.1.61.0.0\lib\native\address-model-64\lib\libboost_regex-vc140-mt-gd-1_61.lib dependencies\lib_x64\boostlib
