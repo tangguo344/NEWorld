66e57d832fe3885a0a8d718365bdca9f57301e19
Null <abc612008@126.com>
2016-08-31 14:56:29 +0800

    Rate meter

---
+++ src/shared/ratemeter.h
@@ -0,0 +1,54 @@
+/*
+* NEWorld: A free game with similar rules to Minecraft.
+* Copyright (C) 2016 NEWorld Team
+*
+* This file is part of NEWorld.
+* NEWorld is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* NEWorld is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with NEWorld.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef RATEMETER_H_
+#define RATEMETER_H_
+
+#include <chrono>
+
+class RateMeter
+{
+public:
+    void refresh()
+    {
+        if (!m_valid)
+        {
+            m_valid = true;
+            return;
+        }
+        auto now = std::chrono::high_resolution_clock::now();
+        m_deltaTime = now - m_lastRefreshTime;
+        m_lastRefreshTime = now;
+    }
+    double getRate()
+    {
+        return 1000.0 / m_deltaTime.count();
+    }
+    double getDeltaTimeMs()
+    {
+        return m_deltaTime.count();
+    }
+
+private:
+    std::chrono::time_point<std::chrono::steady_clock> m_lastRefreshTime;
+    std::chrono::duration<double, std::milli> m_deltaTime;
+    bool m_valid = false;
+};
+
+#endif // !RATEMETER_H_
