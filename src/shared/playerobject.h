56a4292df96abcbe7886ed1ff72310a91382407b
Null <abc612008@126.com>
2016-10-16 18:46:07 +0800

    improve

--- src/shared/playerobject.h
+++ src/shared/playerobject.h
@@ -22,44 +22,42 @@
 
 #include "object.h"
 
-const double PlayerHeight = 1.6;
-const double PlayerWidth = 0.6;
-const double PlayerSpeed = 0.2;
-const Vec3d PlayerHitboxSize = Vec3d(PlayerWidth, PlayerHeight, PlayerWidth);
-
 class PlayerObject : public Object
 {
 public:
-    PlayerObject(const World* world)
-        : Object(world, Vec3d(), Vec3d(), Vec3d(1.0), AABB(-PlayerHitboxSize / 2.0, PlayerHitboxSize / 2.0))
-    {
-    }
+    PlayerObject()
+        : Object(Vec3d(), Vec3d(), Vec3d(1.0), AABB(-m_hitboxSize / 2.0, m_hitboxSize / 2.0)) {}
 
-    PlayerObject(const Object& obj) : Object(obj)
-    {
-    }
+    PlayerObject(const Object& obj) : Object(obj) {}
 
-    ~PlayerObject()
-    {
-    }
+    ~PlayerObject() {}
 
-    void rotate(const Vec3d& rotation)
-    {
-        m_rotation += rotation;
-    }
+    void rotate(const Vec3d& rotation) { m_rotation += rotation; }
 
-    void setDirection(const Vec3d& direction)
-    {
-        m_rotation = direction;
-    }
+    void setDirection(const Vec3d& direction) { m_rotation = direction; }
+    Vec3d getDirection() const { return m_rotation;}
 
-    Vec3d getDirection() const
-    {
-        return m_rotation;
-    }
+    void setHeight(double height) { m_height = height; flushHitbox(); }
+    double getHeight() const { return m_height; }
+
+    void setWidth(double width) { m_width = width; flushHitbox(); }
+    double setWidth() const { return m_width; }
+
+    void setSpeed(double speed) { m_speed = speed; }
+    double getSpeed() const { return m_speed; }
 
 private:
+    void flushHitbox() {
+        m_hitboxSize = Vec3d(m_width, m_height, m_width);
+        m_hitbox = AABB(-m_hitboxSize / 2.0, m_hitboxSize / 2.0);
+    }
     Vec3d m_direction; // Body direction, head direction is `m_rotation` in class Object
+    Vec3d m_direction; // Body direction, head direction is `m_rotation` in class Object
+
+    double m_height = 1.6;
+    double m_width = 0.6;
+    double m_speed = 0.2;
+    Vec3d m_hitboxSize = Vec3d(m_width, m_height, m_width);
 };
 
 #endif
