7f842f0f2633ed4f8477f4daa816cbce1d8628b4
Null <abc612008@126.com>
2016-10-21 19:33:28 +0800

    collect opengl version

--- src/WindowsLauncher/Def.cs
+++ src/WindowsLauncher/Def.cs
@@ -9,9 +9,11 @@ namespace NEWorldLauncher
     {
         public const string gamesPath = "NEWorld";
         public const string exeName = "NEWorld.exe";
-        // Unzipper
         public const string tempFile = "NEWorld/temp.exe";
-        // MainForm.refreshDownloadList();
-        public const string downloadListUri = "http://pan.plyz.net/d.asp?u=3693237900&p=list.txt";
+        public const string downloadListUri = "http://www.infinideas.org/neworld/releases.txt";
+        public const string glewinfo = "glewinfo.exe";
+        public const string glewinfoOutput = "glewinfo.txt";
+        public const string reportAddress = "http://www.infinideas.org/opengl.php";
+        public const string statSettings = "stat.txt";
     }
 }
