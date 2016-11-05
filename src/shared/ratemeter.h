9b3aaced57603cd320e9dd527d2e367cca329d2a
Null <abc612008@126.com>
2016-11-05 08:01:34 +0800

    more precision ratemeter

--- src/shared/ratemeter.h
+++ src/shared/ratemeter.h
@@ -22,11 +22,20 @@
 
 #include <chrono>
 #include <thread>
+#include "common.h"
 
 class RateMeter
 {
 public:
-    explicit RateMeter(int limit = -1):mLimit(limit) {}
+    explicit RateMeter(int limit = -1) : mLastRefreshTime(getTimeNow()), mLimit(limit)
+    {
+#ifdef NEWORLD_USE_WINAPI
+        LARGE_INTEGER num;
+        QueryPerformanceFrequency(&num);
+        mFrequency = num.QuadPart;
+#endif
+    }
+
     void refresh()
     {
         if (!mValid)
@@ -34,41 +43,80 @@ public:
             mValid = true;
             return;
         }
-        auto now = std::chrono::steady_clock::now();
+        auto now = getTimeNow();
+#ifndef NEWORLD_USE_WINAPI
         mDeltaTime = std::chrono::duration_cast<std::chrono::milliseconds>(now - mLastRefreshTime).count();
+#else
+        mDeltaTime = now - mLastRefreshTime;
+#endif
         mLastRefreshTime = now;
     }
     double getRate() const
     {
-        return 1000.0 / mDeltaTime;
+#ifndef NEWORLD_USE_WINAPI
+        return 1000 / mDeltaTime;
+#else
+        return 1.0 / mDeltaTime * mFrequency;
+#endif
     }
     long long getDeltaTimeMs() const
     {
+#ifndef NEWORLD_USE_WINAPI
         return mDeltaTime;
+#else
+        return mDeltaTime / mFrequency;
+#endif
     }
     // notice that this function will not call refresh()!
     void wait() const
     {
         if (mLimit == 0) return;
-        auto now = std::chrono::steady_clock::now();
+        auto now = getTimeNow();
+#ifndef NEWORLD_USE_WINAPI
         auto stdDelta = std::chrono::milliseconds(1000 / mLimit);
         auto deltaTime = std::chrono::duration_cast<std::chrono::milliseconds>(now - mLastRefreshTime);
         if (stdDelta > deltaTime) std::this_thread::sleep_for(stdDelta - deltaTime);
+#else
+        auto stdDelta = 1000 / mLimit; //ms
+        auto deltaTime = static_cast<double>(now - mLastRefreshTime) / mFrequency * 1000;
+        if (stdDelta > deltaTime) Sleep(static_cast<unsigned long>(stdDelta - deltaTime));
+#endif
     }
 
     // notice that this function will not call refresh()!
     bool shouldRun() const
     {
         if (mLimit == 0) return true;
-        auto now = std::chrono::steady_clock::now();
+        auto now = getTimeNow();
+#ifndef NEWORLD_USE_WINAPI
         auto stdDelta = std::chrono::milliseconds(1000 / mLimit);
         auto deltaTime = std::chrono::duration_cast<std::chrono::milliseconds>(now - mLastRefreshTime);
+#else
+        auto stdDelta = 1000.0 / mLimit; //ms
+        auto deltaTime = static_cast<double>(now - mLastRefreshTime) / mFrequency * 1000; //ms
+#endif
         return stdDelta <= deltaTime;
     }
 
 private:
+#ifndef NEWORLD_USE_WINAPI
     std::chrono::steady_clock::time_point mLastRefreshTime;
     long long mDeltaTime = 0ll;
+    static std::chrono::steady_clock::time_point getTimeNow()
+    {
+        return std::chrono::steady_clock::now();
+    }
+#else
+    static __int64 getTimeNow()
+    {
+        LARGE_INTEGER num;
+        QueryPerformanceCounter(&num);
+        return num.QuadPart;
+    }
+    __int64 mLastRefreshTime;
+    __int64 mFrequency;
+    __int64 mDeltaTime = 0ll;
+#endif
     int mLimit = 0ll;
     bool mValid = false;
 };
