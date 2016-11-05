dacb2dc0126a49abdec3e09ceec4c3bcf725b9cd
Miigon <Miigon@users.noreply.github.com>
2016-11-05 12:18:45 +0800

    Format make_fb.py

--- src/protocol/make_fb.py
+++ src/protocol/make_fb.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
-# Works on python2.7 and python3.5,older version may work,too.
+# Works on CPython2.7 and CPython3.5,older version may work,too.
 
-import os		   
-import sys 
+import os
+import sys
 
 print("* Building fbs into './gen' ...")
 
@@ -16,17 +16,18 @@ if flatc_path!="" and not flatc_path[-1] in ["\\","/"]: flatc_path+="/"
 all_protocols=[]
 
 for dirpath, dirs, files in os.walk("."):
-	for filename in files:
-		name,suffix = os.path.splitext(filename)
-		if suffix == ".fbs":
-			type = dirpath.replace("."+os.sep,"")
-			cmd = '%sflatc -c -o "./gen/%s" "./%s/%s"'%(flatc_path,type,type,filename)
-			all_protocols.append(type+name.split("-")[1])
-			print(cmd)
-			os.system(cmd)
-			fbsList.append(dirpath + sep + filename)
+    for filename in files:
+    name,suffix = os.path.splitext(filename)
+        if suffix == ".fbs":
+            type = dirpath.replace("."+os.sep,"")
+            cmd = '%sflatc -c -o "./gen/%s" "./%s/%s"'%(flatc_path,type,type,filename)
+            all_protocols.append(type+name.split("-")[1])
+            print(cmd)
+            os.system(cmd)
+            fbsList.append(dirpath + sep + filename)
 
 print("* Generating 'protocol.h' ...")
+
 if not os.path.exists('./gen/'): os.mkdir('./gen/')
 out = open('./gen/protocol.h','w')
 out.write("""// A part of NEWorld, a free game with similar game rules to Minecraft
@@ -36,13 +37,15 @@ out.write("""// A part of NEWorld, a free game with similar game rules to Minecr
 #define _PROTOCOL_H_
 #include <raknet/MessageIdentifiers.h>
 """)
+
 for f in fbsList:
-	out.write("#include \"" + f.replace(".fbs","") + "_generated.h\"\n")
+    out.write("#include \"" + f.replace(".fbs","") + "_generated.h\"\n")
 out.write("\n")
 out.write("enum class Identifier {Unknown = ID_USER_PACKET_ENUM, " + ", ".join(all_protocols) + ", EndIdentifier};\n")
 out.write("template <class Type> inline Identifier packetType2Id() {return Identifier::Unknown;}\n")
 for protocol in all_protocols:
-	out.write("template <> inline Identifier packetType2Id<%s::%s>() { return Identifier::%s; }\n"%(protocol[:3],protocol[3:],protocol));
+    out.write("template <> inline Identifier packetType2Id<%s::%s>() { return Identifier::%s; }\n"%(protocol[:3],protocol[3:],protocol));
 out.write("#endif\n")
 out.close()
+
 print("* Done.")
