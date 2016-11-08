ff11d465c6daab62e41ba2eb527512ebed34bcb8
Null <abc612008@126.com>
2016-11-08 19:42:16 +0800

    fix some unexcpeted warnings

--- src/client/network.h
+++ src/client/network.h
@@ -1,3 +1,21 @@
+/*
+* NEWorld: A free game with similar rules to Minecraft.
+* Copyright (C) 2016 NEWorld Team
+*
+* This file is part of NEWorld.
+* NEWorld is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* NEWorld is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with NEWorld.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef NETWORK_H__
 #define NETWORK_H__
 
@@ -35,6 +53,7 @@ private:
     std::promise<void> mConnected;
     RakNet::SystemAddress mAddr;
     std::function<void(Identifier, unsigned char*)> mUserDataCallback;
+    std::atomic<bool> mUserClosed = false;
 };
 
 #endif // NETWORK_H__
