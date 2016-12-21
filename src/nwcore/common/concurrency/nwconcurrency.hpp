3ca8f188c6378f0f68d603a8ede29221622efed3
DREAMWORLDVOID <yshliu0321@icloud.com>
2016-12-21 16:20:18 +0800

    Ctor

--- src/nwcore/common/concurrency/nwconcurrency.hpp
+++ src/nwcore/common/concurrency/nwconcurrency.hpp
@@ -4,16 +4,21 @@ class Task
 {
 public:
 	enum class Priority { VeryHigh, High, Normal, Low, VeryLow };
-	virtual void execute() = 0;
+	Task(std::chrono::steady_clock::duration duration, Priority priority)
+		:mDuration(duration), mPriority(priority) {}
 	virtual ~Task() = default;
+	virtual void execute() = 0;
 private:
-	std::chrono::duration mDuration;
+	std::chrono::steady_clock::duration mDuration;
+	Priority mPriority;
 };
 
 class FunctionTask : public Task
 {
 public:
-	FunctionTask(const std::function<void()>& callable) :mCallable(callable) {}
+	FunctionTask(std::chrono::steady_clock::duration duration, Priority priority,
+		const std::function<void()>& callable) :
+		mCallable(callable), Task(duration, priority) {}
 	virtual void execute() override { mCallable(); }
 private:
 	std::function<void()> mCallable;
